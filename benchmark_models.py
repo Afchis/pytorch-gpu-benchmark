5f6c009354c857fca556520a72732cae524f4381
CharlseChen <charleschen2013@163.com>
2019-03-22 14:06:01 +0800

    some edit

--- benchmark_models.py
+++ benchmark_models.py
@@ -1,43 +1,63 @@
 """Compare speed of different models with batch size 16"""
 import torch
-from torchvision.models import resnet, densenet, vgg, squeezenet,inception
+from torchvision.models import resnet, densenet, vgg, squeezenet, inception
 from torch.autograd import Variable
 from info_utils import print_info
 import torch.nn as nn
 import time
 import pandas
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 import argparse
 import os
 from plot import *
+import torch.backends.cudnn as cudnn
 
 print_info()
 
 MODEL_LIST = {
     resnet: resnet.__all__[1:],
-    densenet: densenet.__all__[1:],
+    # densenet: densenet.__all__[1:],
     squeezenet: squeezenet.__all__[1:],
     vgg: vgg.__all__[5:]
 }
 
-precision=["single","half",'double']
-device_name=torch.cuda.get_device_name(0)
+precision = ["single", "half", 'double']
+device_name = torch.cuda.get_device_name(0)
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch Benchmarking')
-parser.add_argument('--WARM_UP','-w', type=int,default=5, required=False, help="Num of warm up")
-parser.add_argument('--NUM_TEST','-n', type=int,default=50,required=False, help="Num of Test")
-parser.add_argument('--BATCH_SIZE','-b', type=int, default=20, required=False, help='Num of batch size')
-parser.add_argument('--NUM_CLASSES','-c', type=int, default=1000, required=False, help='Num of class')
-parser.add_argument('--NUM_GPU','-g', type=int, default=1, required=False, help='Num of class')
+parser.add_argument('--WARM_UP', '-w', type=int, default=5, required=False, help="Num of warm up")
+parser.add_argument('--NUM_TEST', '-n', type=int, default=50, required=False, help="Num of Test")
+parser.add_argument('--BATCH_SIZE', '-b', type=int, default=20, required=False, help='Num of batch size')
+parser.add_argument('--NUM_CLASSES', '-c', type=int, default=1000, required=False, help='Num of class')
+parser.add_argument('--NUM_GPU', '-g', type=int, default=1, required=False, help='Num of class')
 
 args = parser.parse_args()
-device_name+='_'+str(args.NUM_GPU)+'_gpus_'
-args.BATCH_SIZE*=args.NUM_GPU
+device_name += '_' + str(args.NUM_GPU) + '_gpus_'
+args.BATCH_SIZE *= args.NUM_GPU
 torch.backends.cudnn.benchmark = True
+
+
+class data_set(Dataset):
+    def __init__(self):
+        super(data_set, self).__init__()
+
+    def __getitem__(self, item):
+        img = torch.randn(3, 224, 224)
+        target = torch.LongTensor(1).random_(args.NUM_CLASSES).squeeze()
+        return target, img
+
+    def __len__(self):
+        return 256
+
+
 def train(type='single'):
     """use fake image for training speed test"""
     img = Variable(torch.randn(args.BATCH_SIZE, 3, 224, 224)).cuda()
+    # cudnn.benchmark = True
     target = Variable(torch.LongTensor(args.BATCH_SIZE).random_(args.NUM_CLASSES)).cuda()
+    DL = DataLoader(data_set(), batch_size=16, num_workers=8, pin_memory=False)
     criterion = nn.CrossEntropyLoss()
     benchmark = {}
     for model_type in MODEL_LIST.keys():
@@ -46,19 +66,21 @@ def train(type='single'):
             if args.NUM_GPU > 1:
                 model = nn.DataParallel(model)
             if type is 'double':
-                model=model.double()
-                img=img.double()
+                model = model.double()
+                img = img.double()
             elif type is 'single':
-                model=model.float()
-                img=img.float()
+                model = model.float()
+                img = img.float()
             elif type is 'half':
-                model=model.half()
-                img=img.half()
+                model = model.half()
+                img = img.half()
             model.cuda()
             model.train()
             durations = []
-            print('Benchmarking Training '+type+' precision type %s' % (model_name))
-            for step in range(args.WARM_UP + args.NUM_TEST):
+            print('Benchmarking Training ' + type + ' precision type %s' % (model_name))
+            for i, (t, img) in enumerate(DL):
+                target = t.cuda()
+                img = img.cuda()
                 torch.cuda.synchronize()
                 start = time.time()
                 model.zero_grad()
@@ -67,11 +89,13 @@ def train(type='single'):
                 loss.backward()
                 torch.cuda.synchronize()
                 end = time.time()
-                if step >= args.WARM_UP:
-                    durations.append((end - start)*1000)
+                print('time:{:.8f}'.format(end - start))
+                if i >= args.WARM_UP:
+                    durations.append((end - start) * 1000)
             del model
             benchmark[model_name] = durations
     return benchmark
+
 
 def inference(type='single'):
     benchmark = {}
@@ -83,42 +107,42 @@ def inference(type='single'):
                 if args.NUM_GPU > 1:
                     model = nn.DataParallel(model)
                 if type is 'double':
-                    model=model.double()
-                    img=img.double()
+                    model = model.double()
+                    img = img.double()
                 elif type is 'single':
-                    model=model.float()
-                    img=img.float()
+                    model = model.float()
+                    img = img.float()
                 elif type is 'half':
-                    model=model.half()
-                    img=img.half()
+                    model = model.half()
+                    img = img.half()
                 model.cuda()
                 model.eval()
                 durations = []
-                print('Benchmarking Inference '+type+' precision type %s ' % (model_name))
+                print('Benchmarking Inference ' + type + ' precision type %s ' % (model_name))
                 for step in range(args.WARM_UP + args.NUM_TEST):
-                    torch.cuda.synchronize()
+                    # torch.cuda.synchronize()
                     start = time.time()
                     model.forward(img)
-                    torch.cuda.synchronize()
+                    # torch.cuda.synchronize()
                     end = time.time()
                     if step >= args.WARM_UP:
-                        durations.append((end - start)*1000)
+                        durations.append((end - start) * 1000)
                 del model
                 benchmark[model_name] = durations
     return benchmark
-
 
 
 if __name__ == '__main__':
     os.makedirs('results', exist_ok=True)
     for i in precision:
         training_benchmark = pandas.DataFrame(train(i))
-        training_benchmark.to_csv('results/'+device_name+"_"+i+'_model_training_benchmark.csv', index=False)
+        print(training_benchmark)
+        training_benchmark.to_csv('results/' + device_name + "_" + i + '_model_training_benchmark.csv', index=False)
         inference_benchmark = pandas.DataFrame(inference(i))
-        inference_benchmark.to_csv('results/'+device_name+"_"+i+'_model_inference_benchmark.csv', index=False)
-    train=arr_train()
-    inference=arr_inference()
+        print(inference_benchmark)
+        inference_benchmark.to_csv('results/' + device_name + "_" + i + '_model_inference_benchmark.csv', index=False)
+    train = arr_train()
+    inference = arr_inference()
 
-
-    total_model(train,device_name)
-    total_model(inference,device_name)
+    total_model(train, device_name)
+    total_model(inference, device_name)
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+cd /Users/Charles/Code/pytorch-gpu-benchmark
+source activate DL3.7
 python3 benchmark_models.py
